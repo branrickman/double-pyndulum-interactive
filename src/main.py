187fa9364e1c7efee63a36c2785578eeb9382a10
Bryan Brickman <bryanlbrick@gmail.com>
2021-09-16 14:42:45 -0500

    Adds FPS, coloring, consolidates pendulum class, adds color-shift when paused, adds trails and trail control, adds pendulum setting by click

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
 pygame.display.set_caption("Double Pyndulum")
 clock = pygame.time.Clock()
 FPS = 50
+show_FPS = True
 
 # pymunk variables
 space = pymunk.Space()
@@ -19,6 +20,7 @@ space.gravity = (0, -900)
 
 # colors
 WHITE = (255, 255, 255)
+GREY = (200, 200, 200)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 ORANGE = (255, 255, 0)
@@ -44,7 +46,8 @@ def render_text(text, position, color=BLACK):
 
 
 def print_fps():
-    render_text(str(f'FPS: {int(clock.get_fps())}'), (10, 10))  # https://stackoverflow.com/questions/67946230/show-fps-in-pygame
+    render_text(str(f'FPS: {int(clock.get_fps())}'),
+                (10, 10))  # https://stackoverflow.com/questions/67946230/show-fps-in-pygame
 
 
 class PendulumConnector:
@@ -75,6 +78,8 @@ class PendulumPoint:
         self.shape.density = 1
         self.shape.elasticity = 1
         self.shape.collision_type = 2
+        self.position_log = []
+        self.position_trail_radius = 5
 
         self.radius = 10
         self.color = color_list[number]
@@ -82,7 +87,10 @@ class PendulumPoint:
         space.add(self.body, self.shape)
 
     def draw(self):
-        pygame.draw.circle(screen, self.color, convert_coords(self.body.position), self.radius)
+        converted_position = convert_coords(self.body.position)
+        self.position_log.append(converted_position)
+        pygame.draw.circle(screen, self.color, converted_position, self.radius)
+
 
 class Pendulum:
     def __init__(self, x1, y1, x2, y2, number, x3=(SCREEN_WIDTH // 2), y3=(SCREEN_HEIGHT // 2), mode=0):
@@ -90,12 +98,23 @@ class Pendulum:
         self.pend_point2 = PendulumPoint(x2, y2, number + 4)
         self.pend_conn1 = PendulumConnector(self.pend_point1.body, (x3, y3), link_type='static_body')
         self.pend_conn2 = PendulumConnector(self.pend_point1.body, self.pend_point2.body, link_type='body')
+        self.static_x = x3
+        self.static_y = y3
         self.mode = mode
+        self.draw_trail = True
 
     def draw(self):
         if self.mode == 1:
             self.pend_conn1.draw()
             self.pend_conn2.draw()
+        if self.draw_trail:
+            for i in range(len(self.pend_point1.position_log)):
+                pygame.draw.circle(screen, self.pend_point1.color, self.pend_point1.position_log[i],
+                                   self.pend_point1.position_trail_radius)
+            for i in range(len(self.pend_point2.position_log)):
+                pygame.draw.circle(screen, self.pend_point2.color, self.pend_point2.position_log[i],
+                                   self.pend_point2.position_trail_radius)
+
         self.pend_point1.draw()
         self.pend_point2.draw()
 
@@ -135,31 +154,40 @@ while run:
                 FPS += 1
                 print(f'FPS increased to {FPS}')
             if event.key == pygame.K_r:
-                #initialize_pendulums()
+                # initialize_pendulums()
                 print(f'Pendulums reset')
-                # TODO Implement interactive pendulam mechanic
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-        #     print("click")
-        #     pos1 = pygame.mouse.get_pos()
-        #     pos2 = (-1, -1)
-        #     while pos2 == (-1, -1):
-        #         print(f'{pos2}')
-        #         for subevent in pygame.event.get():
-        #             if event.type == pygame.MOUSEBUTTONDOWN:
-        #                 pos2 = pygame.mouse.get_pos()
-        #     pendulum = Pendulum(pos1, pos2, len(pendulum_group) + 1)
-        #     pendulum_group.append(pendulum)
-        #     print(f'{pos2}')
-    screen.fill(WHITE)
+            if event.key == pygame.K_t:
+                for p in pendulum_group:
+                    p.draw_trail = not p.draw_trail
+                print(f'Pendulum trails toggled')
+            if event.key == pygame.K_c:
+                for p in pendulum_group:
+                    p.pend_point1.position_log = []
+                    p.pend_point2.position_log = []
+                print(f'Pendulum trails toggled')
+        mouse_event = pygame.mouse.get_pressed()
+        if mouse_event[0]:
+            pos1 = pygame.mouse.get_pos()
+            pendulum1.pend_point2.body.position = convert_coords(pos1)
+            pendulum1.pend_point1.body.position = convert_coords(
+                ((pos1[0] + pendulum1.static_x) // 2, (pos1[1] + pendulum1.static_y) // 2))
+            # DEBUG
+            # print(f'static: {(pendulum1.static_x, pendulum1.static_y)} \n'
+            #       f'point2: {pos1}\n'
+            #       f'point1: {pendulum1.pend_point1.body.position}')
     if play:
+        screen.fill(WHITE)
         space.step(1 / FPS)
         for pendulum in pendulum_group:
             pendulum.draw()
-        print_fps()
+        if show_FPS:
+            print_fps()
 
     else:
+        screen.fill(GREY)
         for pendulum in pendulum_group:
             pendulum.draw()
-        print_fps()
+        if show_FPS:
+            print_fps()
     clock.tick(FPS)
     pygame.display.update()
